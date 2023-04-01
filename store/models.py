4efdc626fd69d98ceddb34650392aee1177bac61
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-04-01 11:26:12 +0330

    end-3-9

--- store/models.py
+++ store/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount=models.FloatField()
+    #products_set
+
+class Collection(models.Model):
+    title=models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6 , decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
-
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+    # PROTECT -> means if i delited a colection , the products should not Delete
 
 class Customer(models.Model):
 
@@ -42,3 +54,23 @@ class Order(models.Model):
 
     Placed_at = models.DateTimeField(auto_now_add=True)
     Payment_status = models.CharField(max_length=1 ,choices = PAYMENT_STATUS_CHOICES,default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+class OtherItem(models.Model);
+    order = models.ForeignKey(Order,on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity = models.PositiveSmalIntegerField()
+    unit_price = models.DecimalField(max_digits=6 , decimal_places=2)  
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer , on_delete=models.CASCADE, Primary_key=True)
+
+class Cart(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
+    product =models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity= models.PositiveSmallIntegerField()
